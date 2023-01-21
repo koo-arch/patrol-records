a41ab7d57d9492481152ce7e73ee5b1898c26c21
koo-arch <yukshw.ko.0623@gmail.com>
2023-01-21 22:26:31 +0900

    Fix header.php

--- header.php
+++ header.php
@@ -4,17 +4,17 @@
     </header>
     
     <div class="hamburger-menu">
-    <input type="checkbox" id="menu-btn-check">
-    <label for="menu-btn-check" class="menu-btn"><span></span></label>
-    <!--ここからメニュー-->
-    <div class="menu-content">
-        <ul>
-            <li class='content_list'><a href="../users_count/patrol.php">利用人数登録</a></li>
-            <li class='content_list'><a href="../view/table.php">記録表閲覧</a></li>
-            <li class='content_list'><a href="">メニューリンク3</a></li>
-            <li class='logout'><a href="../logout.php">ログアウト</a></li>
-        </ul>
-    </div>
-    <!--ここまでメニュー-->  
+        <input type="checkbox" id="menu-btn-check">
+        <label for="menu-btn-check" class="menu-btn"><span></span></label>
+        <!--ここからメニュー-->
+        <div class="menu-content">
+            <ul>
+                <li class='content_list'><a href="../users_count/patrol.php">利用人数登録</a></li>
+                <li class='content_list'><a href="../view/table.php">記録表閲覧</a></li>
+                <li class='content_list'><a href="">メニューリンク3</a></li>
+                <li class='logout'><a href="../logout.php">ログアウト</a></li>
+            </ul>
+        </div>
+        <!--ここまでメニュー-->  
     </div>
 </div>
